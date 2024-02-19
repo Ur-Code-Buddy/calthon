2d3d26bfca8912adfe8c20f72bf9afbfd3538d8d
Baivab <66193396+Ur-Code-Buddy@users.noreply.github.com>
2024-02-19 12:15:52 +0530

    Update app.py
    made it so that after a point, only 5 digits are shown

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ class Calculator:
         tk.Button(self.master, text="(", font=("Serif", 14), bg="#d3d3d3", bd=1, padx=20, pady=10, command=lambda: self.add_to_window("(")).grid(row=1, column=1, sticky="nsew")
         tk.Button(self.master, text=")", font=("Serif", 14), bg="#d3d3d3", bd=1, padx=20, pady=10, command=lambda: self.add_to_window(")")).grid(row=1, column=2, sticky="nsew")
 
-
         tk.Button(self.master, text="*", font=("Serif", 14), bg="#add8e6", bd=1, padx=20, pady=10, command=lambda: self.add_to_window("*")).grid(row=2, column=3, sticky="nsew")
         tk.Button(self.master, text="/", font=("Serif", 14), bg="#add8e6", bd=1, padx=20, pady=10, command=lambda: self.add_to_window("/")).grid(row=1, column=3, sticky="nsew")
         tk.Button(self.master, text="-", font=("Serif", 14), bg="#add8e6", bd=1, padx=20, pady=10, command=lambda: self.add_to_window("-")).grid(row=3, column=3, sticky="nsew")
@@ -93,6 +92,7 @@ class Calculator:
                 if "/0" in modified_expression:
                     raise ZeroDivisionError
                 result = eval(modified_expression, {}, {"math": math})
+                result = round(result, 5)
                 self.result_var.set(result)
         except ZeroDivisionError:
             messagebox.showerror("Error", "Cannot divide by zero")
